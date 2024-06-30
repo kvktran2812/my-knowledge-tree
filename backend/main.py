b5b08371fafdf7cd63eb572cf45d3cfa6dc3494b
kvktran2812 <kvktran@gmail.com>
2024-06-29 22:00:51 -0400

    some changes and gitignore

--- backend/main.py
+++ backend/main.py
@@ -5,6 +5,7 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
+# root route
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
@@ -13,3 +14,12 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.get("/knowledges/{id}")
+def read_knowledge(id: int):
+    return {
+        "id": id,
+        "data": "some data"
+    }
+
